0a3b0e60607048bac4175ecc7fe80cde5d39a20e
Mohammadfahril <mohammadfahril225@gmail.com>
2022-05-17 00:06:48 +0800

    Ini adalah pesan commit pertama anda

---
+++ app/src/main/java/com/f55119097/tugaspraktikumfahril/ui/detail/DetailActivity.java
@@ -0,0 +1,6 @@
+package com.f55119097.tugaspraktikumfahril.ui.detail;
+
+import android.app.Activity;
+
+public class DetailActivity extends Activity {
+}
---
+++ app/src/main/java/com/f55119097/tugaspraktikumfahril/ui/main/MainActivity.java
@@ -0,0 +1,6 @@
+package com.f55119097.tugaspraktikumfahril.ui.main;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
